cca5abf037992a1e2d2fdf2423d21203612bcf0e
Umer Khan <umerkk164@gmail.com>
2020-04-06 17:44:20 +0500

    Updates

---
+++ precache-manifest.ca901ca80dc3189310c435f4f225e143.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aade6ea988cb6337c56898cb24dc5433",
+    "url": "/prayerjournal/index.html"
+  },
+  {
+    "revision": "7d0707474436ddd86f89",
+    "url": "/prayerjournal/static/css/main.afb4338b.chunk.css"
+  },
+  {
+    "revision": "3b7415a10b319df604c7",
+    "url": "/prayerjournal/static/js/2.97d5406f.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/prayerjournal/static/js/2.97d5406f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0707474436ddd86f89",
+    "url": "/prayerjournal/static/js/main.9c478652.chunk.js"
+  },
+  {
+    "revision": "ebcd8f95a5adf32452c8",
+    "url": "/prayerjournal/static/js/runtime-main.702abb5c.js"
+  },
+  {
+    "revision": "7d02b50b228d18e7da9e43b122aa2d84",
+    "url": "/prayerjournal/static/media/loading.7d02b50b.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prayerjournal/static/media/logo.5d5d9eef.svg"
+  }
+]);
